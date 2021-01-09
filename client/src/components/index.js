c6278a4575b3a69b27a441a787da775dcc6f34a9
Jake Wagner <jkwagneriii@gmail.com>
2021-01-09 15:55:53 -0600

    added particles to the index.js

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,13 +1,34 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Header from './Header'
+import Particles from 'react-particles-js'
+import {makeStyles} from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+    particlesCanvas: {
+        position: 'absolute',
+    }
+})
 
 const Home = () => {
+
+    const classes = useStyles()
+
     return (
-        <div>
+        <>
             <Navbar />
             <Header />
-        </div>
+            <Particles 
+                canvasClassName={classes.particlesCanvas}
+                params={{
+                    particles: {
+                        number: {
+                            value: 45
+                        }
+                    }
+                }}
+            />
+        </>
     )
 }
 
