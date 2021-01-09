ffd695b002cbd823886b9ff543340c52af36159e
Jake Wagner <jkwagneriii@gmail.com>
2021-01-09 13:57:13 -0600

    cleared out the project and installed material ui

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {
+AppBar,
+Toolbar,
+ListItem,
+IconButton,
+ListItemText,
+Avatar,
+Divider,
+List,
+Typography,
+Box    
+} from '@material-ui/core'
+import {
+ArrowBack,
+AssignmentInd,
+Home,
+Apps,
+ContactMail    
+} from '@material-ui/icons'
+
+const Navbar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Navbar
