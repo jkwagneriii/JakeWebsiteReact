aeb9fb309cdb4050af623a0cd6fa6440fcaf441d
Jake Wagner <jkwagneriii@gmail.com>
2021-01-09 14:43:02 -0600

    Navbar component filled out. Going to work on toggle function

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import {makeStyles} from '@material-ui/core/styles'
 import {
 AppBar,
 Toolbar,
 ListItem,
 IconButton,
 ListItemText,
+ListItemIcon,
 Avatar,
 Divider,
 List,
@@ -18,9 +20,63 @@ Home,
 Apps,
 ContactMail    
 } from '@material-ui/icons'
+import avatar from '../images/avatar.JPG'
+
+// CSS STYLING
+const useStyles = makeStyles(theme=>({
+    menuSliderContainer: {
+        width: 250,
+        background: "#ADFFE8",
+        height: "30rem"
+    },
+    avatar: {
+        display: "block",
+        margin: "0.5rem auto",
+        width: theme.spacing(13),
+        height: theme.spacing(13)
+    },
+    listItem: {
+        color: "black"
+    }
+}));
+
+const menuItems = [
+    {
+        listIcon: <Home/>,
+        listText: "Home"
+    },
+    {
+        listIcon: <AssignmentInd/>,
+        listText: "Resume"
+    },
+    {
+        listIcon: <Apps/>,
+        listText: "Portfolio"
+    },
+    {
+        listIcon: <ContactMail/>,
+        listText: "Contact"
+    },
+]
 
 const Navbar = () => {
+    const classes = useStyles()
     return (
+        <>
+        <Box className={classes.menuSliderContainer} component="div">
+            <Avatar className={classes.avatar} src={avatar} alt="Jake Wagner" />
+            <Divider />
+            <List>
+                {menuItems.map((lsItem, key)=>(
+                <ListItem button key={key}>
+                    <ListItemIcon className={classes.listItem}>
+                        {lsItem.listIcon}
+                    </ListItemIcon>
+                    <ListItemText className={classes.listItem} primary={lsItem.listText}/>
+                </ListItem>
+                ))}
+            </List>
+        </Box>
         <Box component="nav">
             <AppBar position="static" style={{background: '#FFAFE0'}}>
                 <Toolbar>
@@ -33,6 +89,7 @@ const Navbar = () => {
                 </Toolbar>
             </AppBar>
         </Box>
+        </>
     )
 }
 
