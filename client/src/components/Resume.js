f24ef130e7f90781cfdee017fad0da67738105e2
Jake Wagner <jkwagneriii@gmail.com>
2021-01-10 16:00:04 -0600

    style fiddling

--- client/src/components/Resume.js
+++ client/src/components/Resume.js
@@ -5,7 +5,8 @@ import Navbar from './Navbar'
 
 const useStyles = makeStyles(theme=>({
     mainContainer: {
-        background: "#b2af97"
+        background: "#b2af97",
+        opacity: "0.75"
     },
     timeLine: {
         position: "relative",
