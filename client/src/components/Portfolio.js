cd17bd18d3873d7f6faaa8ef0650054361fbdc03
Jake Wagner <jkwagneriii@gmail.com>
2021-01-11 12:36:28 -0600

    working on portfolio component. added all giphy and info

--- client/src/components/Portfolio.js
+++ client/src/components/Portfolio.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {makeStyles} from '@material-ui/core/styles'
 import {
 Box,
+Divider,
 Grid,
 Card,
 CardActionArea,
@@ -31,7 +32,9 @@ const Portfolio = () => {
     const classes = useStyles()
     const project1 = "https://giphy.com/embed/7D3CFhXBdpJoiQ58hG"
     const project2 = "https://giphy.com/embed/QQLOmrJI75wfHPm3Gs"
-    
+    const project3 = "https://giphy.com/embed/CveFhs9z9l5bsNmH6O"
+    const project4 = "https://giphy.com/embed/WjVp4SzxmNLzV1l3ax"
+   
     return (
         <>
         <Box component="div" className={classes.mainContainer}>
@@ -48,10 +51,14 @@ const Portfolio = () => {
                             image={project1} />
                             <CardContent>
                                 <Typography gutterBottom variant="h5">
-                                    Project 1
+                                    Smug Sheep
                                 </Typography>
                                 <Typography variant="body2" color="red" component="p">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem ducimus perferendis, autem veritatis eius laborum unde ab veniam sed sapiente dignissimos expedita corporis numquam odio consectetur laudantium? Libero, provident consequatur!
+                                In the year 2020 we all learned a lot about ourselves as well as the world around us. Often, focusing on the negative is easier than focusing on the positive. Because of that, the team at Smug Sheep wanted to provide a space to share the good in your life, no matter how big or small, in a safe anonymous environment. Maybe a stranger bought your groceries or maybe your neighbor just happened to bring in your plants before the big storm...either way it deserves recognition! So hop on over the the Smug Sheep feed, join the flock, and start shearing that negativity away one kind story at a time.
+                                </Typography>
+                                <Divider />
+                                <Typography variant="caption">
+                                This application uses React, JavaScript, Material UI, MongoDB, Node, Express, Particles, and Auth0
                                 </Typography>
                             </CardContent>
                             <CardActions>
@@ -73,11 +80,80 @@ const Portfolio = () => {
                             alt="Project 2"
                             image={project2} />
                             <CardContent>
-                                <Typography gutterBottom variant="h5">
-                                    Project 2
+                            <Typography gutterBottom variant="h5">
+                                    Veggie Vision
                                 </Typography>
                                 <Typography variant="body2" color="red" component="p">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem ducimus perferendis, autem veritatis eius laborum unde ab veniam sed sapiente dignissimos expedita corporis numquam odio consectetur laudantium? Libero, provident consequatur!
+                                Here at Veggie Vision, we understand it can be difficult to know what to do with all those wild
+                                vegetables out there. I mean what is a beet, right? That’s why we have created an application to
+                                help you find the perfect veggies for you. Whether you are working on a an iron deficiency or just
+                                trying to get your kids to eat more greens, Veggie Vision has your back!
+                                </Typography>
+                                <Divider />
+                                <Typography variant="caption">
+                                This application uses Handlebars, JavaScript, Jquery, MySQL, Node, and Bootstrap
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small" color="#758877">
+                                    Share
+                                </Button>
+                                <Button size="small" color="#273b49">
+                                    Live Demo
+                                </Button>
+                            </CardActions>    
+                        </CardActionArea>
+                    </Card>
+                    {/* Project 3  */}
+                    <Card className={classes.cardContainer}>
+                        <CardActionArea>
+                            <CardMedia
+                            frameBorder="0"
+                            component="iframe"
+                            alt="Project 2"
+                            image={project3} />
+                            <CardContent>
+                            <Typography gutterBottom variant="h5">
+                                    What's Your Dog, Dog?
+                                </Typography>
+                                <Typography variant="body2" color="red" component="p">
+                                Here at What's Your Dog, Dog? We are committed to matching you up with your new best friend! We
+                                understand that dogs, just like people, have a wide range of personalities. So, scroll down and
+                                begin your search for a new buddy!
+                                </Typography>
+                                <Divider />
+                                <Typography variant="caption">
+                                This application uses HTML5, PureCSS, and REST API
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small" color="#758877">
+                                    Share
+                                </Button>
+                                <Button size="small" color="#273b49">
+                                    Live Demo
+                                </Button>
+                            </CardActions>    
+                        </CardActionArea>
+                    </Card>
+                    {/* Project 4  */}
+                    <Card className={classes.cardContainer}>
+                        <CardActionArea>
+                            <CardMedia
+                            frameBorder="0"
+                            component="iframe"
+                            alt="Project 2"
+                            image={project4} />
+                            <CardContent>
+                            <Typography gutterBottom variant="h5">
+                                    React Recipes
+                                </Typography>
+                                <Typography variant="body2" color="red" component="p">
+                                This is a simple web application for finding food recipes. Once the application loads, type in an ingredient and press search. You will see 10 recipes that have images, links, and ingredient information. Find something you like and get ready to chow down.
+                                </Typography>
+                                <Divider />
+                                <Typography variant="caption">
+                                This application uses React, UUID, and Particles
                                 </Typography>
                             </CardContent>
                             <CardActions>
