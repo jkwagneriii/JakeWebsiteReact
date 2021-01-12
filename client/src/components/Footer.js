0584d3f4db992c1ccdc5278fbc42363c1c3da0b3
Jake Wagner <jkwagneriii@gmail.com>
2021-01-12 15:01:55 -0600

    footer icons working and looking good

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -10,6 +10,13 @@ const useStyles = makeStyles({
         "& .MuiBottomNavigationAction-root": {
             minWidth: 0,
             maxWidth: "auto"
+        },
+        "& .MuiSvgIcon-root": {
+            fill: "#273b49",
+            "&:hover": {
+                fill: "#efd5b6",
+                fontSize: "1.8rem"
+            }
         }
     }
 })
@@ -22,16 +29,21 @@ const Footer = () => {
             className={classes.root}
             style={{padding: 0}}
             icon={<GitHubIcon/>}
+            href="https://github.com/jkwagneriii"
+            target="_blank"
             />
             <BottomNavigationAction
             className={classes.root}
             style={{padding: 0}}
             icon={<EmailIcon/>}
+            href="mailto:jkwagneriii@gmail.com"
             />
             <BottomNavigationAction
             className={classes.root}
             style={{padding: 0}}
             icon={<LinkedInIcon/>}
+            href="https://www.linkedin.com/in/jake-wagner-iii/"
+            target="_blank"
             />
         </BottomNavigation>
     )
